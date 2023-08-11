d6b0f0039a22d43bb19dfd9baed5dd4f9a168a60
Aaditya Bhatia <aadityabhatia@gmail.com>
2023-08-11 19:26:07 -0400

    added game config option REPLAY_URL_PREFIX

--- src/types.d.ts
+++ src/types.d.ts
@@ -201,6 +201,7 @@ declare namespace RedisData {
 declare namespace Config {
 	interface Game {
 		GAME_SERVER_URL: string
+		REPLAY_URL_PREFIX: string
 		MAX_TURNS: number
 		BOT_ID_PREFIX: string
 		userId: string
